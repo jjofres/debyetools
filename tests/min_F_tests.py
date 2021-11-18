e2855fc1fd9ffdc35b9b004c14467024bec59d36
javier <jovier@gmail.com>
2021-11-18 00:02:55 -0500

    fixing bugs

--- tests/min_F_tests.py
+++ tests/min_F_tests.py
@@ -7,7 +7,7 @@ class FminTestCase(unittest.TestCase):
         self.nu, self.m = 0.31681, 0.026981500000000002
         self.Tmelting = 933
         self.p_EOS = [-3.617047894e+05, 9.929931142e-06, 7.618619745e+10, 4.591924487e+00]
-        self.p_intanh = 0, 1, self.p_EOS[1]
+        self.p_intanh = 0, 1
         self.p_electronic = [3.8027342892e-01, -1.8875015171e-02, 5.3071034596e-04, -7.0100707467e-06]
         self.p_defects = 8.46, 1.69, self.Tmelting, 0.1, self.p_EOS[2],self.p_EOS[1]
         self.p_anh = 0,0,0

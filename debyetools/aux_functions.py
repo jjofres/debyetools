2405d52ef8f43c6ce49580dff44578c804cd577f
Javier <jovier@gmail.com>
2023-03-07 14:08:27 -0500

    update_logging

--- debyetools/aux_functions.py
+++ debyetools/aux_functions.py
@@ -2,6 +2,16 @@ import itertools as it
 import re
 import numpy as np
 
+class logging(object):
+    def __init__(self, *files):
+        self.files = files
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+            f.flush() # If you want the output to be visible immediately
+    def flush(self) :
+        for f in self.files:
+            f.flush()
 def c_types(atom_types):
     """
     returns all the pair types combinations.

2a8f1e9e909b9d27d0e45649ea0abee6782e3a61
Javier <jovier@gmail.com>
2023-05-15 13:53:34 -0400

    update tests

--- tests/pairanalysis_tests.py
+++ tests/pairanalysis_tests.py
@@ -22,7 +22,7 @@ class GenerateCellCoordinatesTestCase(unittest.TestCase):
 
     def test_nl_0(self):
         """ Test fcc crystal of lattice a=1 1x1x1 times and center=(0,0,0) """
-        result = pa_calc.neighbor_list(self.size(1),2,self.center(0),self.basis_vectors,self.primitive_cell(1))
+        result = pa_calc.neighbor_list(self.size(1),self.basis_vectors,self.primitive_cell(1), 2)
         bool_1 = None==np.testing.assert_array_almost_equal(result[0][3:10],np.array([1.87082869,1.87082869,1.87082869,1.87082869,1.87082869,1.58113883,1.87082869]))
         bool_2 = None==np.testing.assert_array_almost_equal(result[1][3:10],np.array([0,3,0,3,0,0,3]))
         bool_3 = None==np.testing.assert_array_almost_equal(result[2][3:10],np.array([2,1,1,1,1,2,2]))
@@ -31,7 +31,7 @@ class GenerateCellCoordinatesTestCase(unittest.TestCase):
 
     def test_pa_0(self):
         """ Test the pair analysis calculation for a fcc crystal up to 7NN"""
-        results =pa_calc.pair_analysis('AABA', self.size(1), 2, self.center(0), self.basis_vectors, self.primitive_cell(1))
+        results =pa_calc.pair_analysis('AABA', 2, self.basis_vectors, self.primitive_cell(1))
         bool_1 = None == np.testing.assert_array_almost_equal(results[0], np.array(
             [0.70710678, 1., 1.22474487, 1.41421356, 1.58113883, 1.73205081, 1.87082869]))
         bool_2 = None == np.testing.assert_array_almost_equal(results[1][1:3,:], np.array([[4.5,0.,1.5],[12.,12.,0.]]))
@@ -41,12 +41,10 @@ class GenerateCellCoordinatesTestCase(unittest.TestCase):
 
     def test_pa_1(self):
         formula = 'AABA'
-        supcell_size = np.array([1,1,1])
         cutoff = 5
-        center = np.array([0,0,0])
         basis_vectors = np.array([[0,0,0],[.5,.5,0],[.5,0,.5],[0,.5,.5]])
         primitive_cell =  np.array([[4, 0, 0], [0, 4, 0], [0, 0, 4]])
-        distances, num_bonds_per_formula, combs_types = pa_calc.pair_analysis(formula, supcell_size, cutoff, center, basis_vectors, primitive_cell)
+        distances, num_bonds_per_formula, combs_types = pa_calc.pair_analysis(formula, cutoff, basis_vectors, primitive_cell)
 
         print('')
         print('distances  | # of pairs per type')
